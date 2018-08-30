107d35c71c62cd055d4c40b8eb8777a6992fe91a
Davide Conzon <conzon@ismb.it>
2018-08-30 11:57:38 +0200

    Fixed the Status

--- src/main/java/eu/cpswarm/optimization/messages/OptimizationReplyMessage.java
+++ src/main/java/eu/cpswarm/optimization/messages/OptimizationReplyMessage.java
@@ -1,15 +1,22 @@
 package eu.cpswarm.optimization.messages;
 
+import com.google.gson.annotations.SerializedName;
+
 public abstract class OptimizationReplyMessage extends Message {
 
 	public enum Status {
-		OK, ERROR
+		@SerializedName("0")
+		OK, 
+		@SerializedName("1")
+		ERROR
 	}
 
+	@SerializedName("status")
 	protected Status operationStatus;
 
 	public OptimizationReplyMessage(String type, String id, Status operationStatus) {
 		super(type, id);
+		this.operationStatus = operationStatus;
 	}
 
 	public Status getOperationStatus() {
