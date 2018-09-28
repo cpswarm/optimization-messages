a2fc1a55f739fcc6a79d5cfb028d1fbd547ab981
Davide Conzon <conzon@ismb.it>
2018-09-28 12:07:21 +0200

    Added SimularConfiguredMessage

--- src/main/java/eu/cpswarm/optimization/messages/SimulatorConfiguredMessage.java
+++ src/main/java/eu/cpswarm/optimization/messages/SimulatorConfiguredMessage.java
@@ -1,14 +1,9 @@
 package eu.cpswarm.optimization.messages;
 
-import com.google.gson.annotations.SerializedName;
-
 public class SimulatorConfiguredMessage extends ReplyMessage {
 
 	public static final String TYPE_NAME = "SimulationConfigured";
 
-	@SerializedName("SID")
-	protected String sid;
-	
 	public SimulatorConfiguredMessage(String id, String description, Status operationStatus) {
 		super(TYPE_NAME, id, description, operationStatus);
 	}
