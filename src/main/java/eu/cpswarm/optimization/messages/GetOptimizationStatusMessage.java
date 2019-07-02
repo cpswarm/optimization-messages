bb628166916a43595310b7381bbef1e4f9517999
Davide Conzon <conzon@ismb.it>
2019-07-02 16:21:37 +0200

    minor fix

--- src/main/java/eu/cpswarm/optimization/messages/GetOptimizationStatusMessage.java
+++ src/main/java/eu/cpswarm/optimization/messages/GetOptimizationStatusMessage.java
@@ -3,7 +3,7 @@ package eu.cpswarm.optimization.messages;
 public class GetOptimizationStatusMessage extends Message {
 	public static final String TYPE_NAME = "GetOptimizationStatus";
 
-	public GetOptimizationStatusMessage(String oid, String TYPE_NAME) {
+	public GetOptimizationStatusMessage(String oid) {
 		super(TYPE_NAME, oid);
 	}
 }
