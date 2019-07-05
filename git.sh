abe31e1770f5377ceba490072d56bdf69c0f053a
Rui Zhao <rzhao@ismb.it>
2019-07-05 16:13:47 +0200

    update OptimizationStatusMessage

---
+++ git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add . 
+git commit -m 'update OptimizationStatusMessage'
+git push -u origin api-2.0
--- src/main/java/eu/cpswarm/optimization/messages/OptimizationStatusMessage.java
+++ src/main/java/eu/cpswarm/optimization/messages/OptimizationStatusMessage.java
@@ -6,16 +6,18 @@ public class OptimizationStatusMessage extends Message {
 	public static final String TYPE_NAME = "OptimizationStatus";
 
 	public enum Status {
-		@SerializedName("ErrorBadConfiguration")
+		@SerializedName("ErrorBadConfiguration")  // optimization is not actived
 		ERROR_BAD_CONFIGURATION,
-		@SerializedName("Running")
+		@SerializedName("Started")   // reply to StartOptimization when received in first time, maybe remove the OptimizationToolConfigured class  
+		STARTED,
+		@SerializedName("Running")   // running for some time, with a progress
 		RUNNING,
-		@SerializedName("ErrorOptimizationFailed")
+		@SerializedName("ErrorOptimizationFailed")     // error appeared
 		ERROR_OPTIMIZAZION_FAILED,
-		@SerializedName("Stopped")
-		STOPPED,
-		@SerializedName("Complete")
-		COMPLETE
+		@SerializedName("Cancelled")    //  request from SOO to cancel it without error, stopped
+		CANCELLED,
+		@SerializedName("Completed")
+		COMPLETED
 	}
 	
 	@SerializedName("status")
@@ -28,8 +30,9 @@ public class OptimizationStatusMessage extends Message {
 	@SerializedName("bestController")
 	protected String bestController;
 	
-	public OptimizationStatusMessage(String oid, String TYPE_NAME, Status operationStatus, double bestFitnessValue, String bestController) {
+	public OptimizationStatusMessage(String oid, String TYPE_NAME, double progress, Status operationStatus, double bestFitnessValue, String bestController) {
 		super(TYPE_NAME, oid);
+		this.progress = progress;
 		this.operationStatus = operationStatus;
 		this.bestFitnessValue = bestFitnessValue;
 		this.bestController = bestController;
