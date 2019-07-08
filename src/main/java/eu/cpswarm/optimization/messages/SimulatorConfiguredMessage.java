d411849ee819b2b96f561d811e5cd447f3019291
Rui Zhao <rzhao@ismb.it>
2019-07-08 16:01:30 +0200

    update simulatorconfigured

--- src/main/java/eu/cpswarm/optimization/messages/SimulatorConfiguredMessage.java
+++ src/main/java/eu/cpswarm/optimization/messages/SimulatorConfiguredMessage.java
@@ -2,7 +2,7 @@ package eu.cpswarm.optimization.messages;
 
 public class SimulatorConfiguredMessage extends ReplyMessage {
 
-	public static final String TYPE_NAME = "SimulationConfigured";
+	public static final String TYPE_NAME = "SimulatorConfigured";
 
 	public SimulatorConfiguredMessage(String oid, Boolean success) {
 		super(TYPE_NAME, oid, success);
